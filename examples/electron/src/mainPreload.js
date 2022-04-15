1b6bba38e971cdd4246ffc1367de7daa1dca49d9
g-y-t <99533631+g-y-t@users.noreply.github.com>
2022-04-15 10:56:50 +0800

    electron demo增加设置启动参数的api (#3)

--- examples/electron/src/mainPreload.js
+++ examples/electron/src/mainPreload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld(
   'finclip',
   {
-    open: () => ipcRenderer.send('OPEN_FINCLIP_WINDOW'),
+    open: (payload) => ipcRenderer.send('OPEN_FINCLIP_WINDOW', payload),
     close: () => ipcRenderer.send('CLOSE_FINCLIP_WINDOW'),
     setPosition: (payload) => ipcRenderer.send('SET_FINCLIP_POSITION', payload),
   }
--- examples/electron/src/mainScript.js
+++ examples/electron/src/mainScript.js
@@ -1,5 +1,11 @@
 window.openFinClipWindow = () => {
-  finclip.open();
+  const domain = document.getElementById('domain').value;
+  const appkey = document.getElementById('appkey').value;
+  const appid = document.getElementById('appid').value;
+  const secret = document.getElementById('secret').value;
+  finclip.open({
+    domain, appkey, appid, secret,
+  });
 };
 
 window.closeFinClipWindow = () => {
