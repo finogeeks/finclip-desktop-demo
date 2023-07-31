0388760736b798f145c5c5a584efd9802f1cc0fc
Gyt <igyteng@gmail.com>
2023-07-31 21:00:08 +0800

    feat: 采用koffi库的electron demo

---
+++ examples/electron/src/finclip.js
@@ -0,0 +1,38 @@
+const koffi = require('koffi');
+
+const finclip = {};
+let lib;
+
+const loadLibrary = libraryPath => {
+  lib = koffi.load(libraryPath);
+  koffi.pointer('POINT', koffi.opaque());
+  const lifecycleCallback = koffi.callback('lifecycleCallback', 'void', ['int', 'string', 'void*']);
+  const customApiCallback = koffi.callback('customApiCallback', 'void', ['string', 'string', 'string', 'int']);
+
+  finclip.finclip_create_params = lib.func('finclip_create_params', 'POINT', []);
+  finclip.finclip_destory_params = lib.func('finclip_destory_params', 'int', ['POINT']);
+  finclip.finclip_params_set = lib.func('finclip_params_set', 'int', ['POINT', 'string', 'string']);
+  finclip.finclip_init_with_config = lib.func('finclip_init_with_config', 'int', ['string', 'POINT']);
+  finclip.finclip_start_applet = lib.func('finclip_start_applet', 'int', ['string', 'string']);
+  finclip.finclip_start_applet_embed = lib.func('finclip_start_applet_embed', 'int', ['string', 'string', 'POINT', 'int']);
+  finclip.finclip_close_applet = lib.func('finclip_close_applet', 'int', ['string']);
+  finclip.finclip_set_position = lib.func('finclip_set_position', 'int', ['string', 'int', 'int', 'int', 'int']);
+  finclip.finclip_callback_success = lib.func('finclip_callback_success', 'int', ['int', 'POINT']);
+  finclip.finclip_callback_failure = lib.func('finclip_callback_failure', 'int', ['int', 'POINT']);
+
+  finclip.finclip_register_lifecycle = (appid, type, fn, data) => {
+    const callback = koffi.register(fn, koffi.pointer(lifecycleCallback));
+    const finclip_register_lifecycle = lib.func('finclip_register_lifecycle', 'int', ['string', 'int', koffi.pointer(lifecycleCallback), 'void*']);
+    return finclip_register_lifecycle(appid, type, callback, data);
+  };
+
+  finclip.finclip_register_api = (type, name, fn, data) => {
+    const callback = koffi.register(fn, koffi.pointer(customApiCallback));
+    const finclip_register_api = lib.func('finclip_register_api', 'int', ['int', 'string', koffi.pointer(customApiCallback), 'POINT']);
+    return finclip_register_api(type, name, callback, data);
+  };
+};
+
+finclip.load_library = loadLibrary;
+
+module.exports = finclip;
--- src/npm/README.md
+++
@@ -1,8 +0,0 @@
-### 编译
-
-1. npm run configure
-2. npm run build
-
-### 运行
-
-node index.js
