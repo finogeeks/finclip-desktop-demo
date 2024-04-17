7a617df7e5fa97eb72085e1f03f9be35ad5d8f8d
GYT-THINKBOOK <igyteng@gmail.com>
2024-04-17 15:49:04 +0800

    fix: add more function in electron demo

--- examples/electron/src/mainPreload.js
+++ examples/electron/src/mainPreload.js
@@ -4,8 +4,6 @@ contextBridge.exposeInMainWorld(
   'finclip',
   {
     open: (payload) => ipcRenderer.send('OPEN_FINCLIP_WINDOW', payload),
-    embed: () => ipcRenderer.send('EMBED_FINCLIP_WINDOW'),
     close: () => ipcRenderer.send('CLOSE_FINCLIP_WINDOW'),
-    setPosition: (payload) => ipcRenderer.send('SET_FINCLIP_POSITION', payload),
   }
 )
